a8a701ac645e9ed2c9efa09f6008492696eb36f0
Rushil Kumar <rukumar333@gmail.com>
2017-01-25 03:49:56 -0500

    Started declaration for deque.

---
+++ assignments/Assignment_1/Deque.hpp
@@ -0,0 +1,40 @@
+#include <assert.h>
+#include <iostream>
+#include <cstdio>
+
+
+struct Deque_DEFINE{
+  size_t length;
+  size_t queue_length;
+  int *array;  
+};
+
+void push_back(Deque_DEFINE *deq, int item) {
+  
+}
+
+void push_front(Deque_DEFINE *deq, int item) {
+  
+}
+
+void pop_front(Deque_DEFINE *deq) {
+  
+}
+
+void pop_back(Deque_DEFINE *deq) {
+  
+}
+
+int front
+
+size_t size(Deque_DEFINE *deq) {
+  return deq->queue_length;
+}
+
+bool empty(Deque_DEFINE *deq) {
+  return deq->queue_length == 0;
+}
+
+
+
+
