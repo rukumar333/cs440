d43674e4ce321cba8ed47202dd20316fd66a3d7a
Rushil Kumar <rukumar333@gmail.com>
2017-02-16 01:31:21 -0500

    Starting assignment 2 map.

---
+++ assignments/Assignment_2/Map.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <utility>
+
+#ifndef CS350_MAP_HPP_
+#define CS350_MAP_HPP_
+
+template <typename Key_T, typename Mapped_T>
+class Map {
+ private:
+  
+ public:
+  class Iterator {
+	
+  };
+
+  class ConstIterator {
+	
+  };
+
+  class ReverseIterator {
+	
+  }
+};
+
+#endif
