92f30b84313305c2897478a63b56c1d63e74ea1a
Rushil Kumar <rukumar333@gmail.com>
2017-05-19 14:29:31 -0400

    Need to implement last dimension order iterator.

--- assignments/Assignment_4/Array.hpp
+++ assignments/Assignment_4/Array.hpp
@@ -17,25 +17,92 @@ namespace cs540 {
 	class LastDimensionMajorIterator;
 	typedef FirstDimensionMajorIterator FMIterator;
 	typedef LastDimensionMajorIterator LMIterator;
+	
 	class FirstDimensionMajorIterator {
 	 private:
-	  // typedef typename NextArray::FirstDimensionMajorIterator NextIterator;
-	  NextArray array[DIM_T];
-	  bool at_end;
-	  // typename Array<T, Dims...>::FirstDimensionMajorIterator next_iterator;
+	  typedef typename NextArray::FirstDimensionMajorIterator NextIterator;
+	  friend class Array<T, DIM_T, Dims...>;
+	  size_t index;
+	  NextIterator next_iterator;
+	  Array<T, DIM_T, Dims...> *outer_ptr;
 	 public:
-	  // FirstDimensionMajorIterator(bool begin, NextArray other_array[DIM_T]) {
-	  // 	if (begin)
-	  // 	  at_end = false;
-	  // 	else
-	  // 	  at_end = true;
-	  // 	// array = other_array;
-	  // }
+	  bool at_end;
+	  FirstDimensionMajorIterator(Array<T, DIM_T, Dims...> &outer_class) {
+		at_end = false;
+		index = 0;
+		outer_ptr = &outer_class;
+		next_iterator = outer_ptr->array[index].fmbegin();
+	  }
+	  
 	  FirstDimensionMajorIterator() {
 		at_end = false;
+		index = 0;
+		outer_ptr = nullptr;
 	  }
+	  
+	  FirstDimensionMajorIterator(const FMIterator &other) {
+		at_end = other.at_end;
+		next_iterator = other.next_iterator;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+	  }
+	  
+	  FirstDimensionMajorIterator &operator=(const FMIterator &other) {
+		at_end = other.at_end;
+		next_iterator = other.next_iterator;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+		return &this;
+	  }
+
+	  friend bool operator==(const FMIterator &first, const FMIterator &second) {
+		return (first.index == second.index) && (first.outer_ptr == second.outer_ptr)
+		  && (first.next_iterator = second.next_iterator);
+	  }
+
+	  friend bool operator!=(const FMIterator &first, const FMIterator &second) {
+		return !(first == second);
+	  }
+	  
+	  FirstDimensionMajorIterator &operator++() {
+		if (next_iterator.at_end != true) {
+		  ++ next_iterator;
+		} else {
+		  if (at_end != true) {
+			++ index;
+			if (index == DIM_T) {
+			  at_end = true;
+			} else {
+			  next_iterator = outer_ptr->array[index].fmbegin();
+			}
+		  }
+		}
+		return *this;
+	  }
+	  
+	  FirstDimensionMajorIterator operator++(int) {
+		auto it = *this;
+		if (next_iterator.at_end != true) {
+		  ++ next_iterator;
+		} else {
+		  if (at_end != true) {
+			++ index;
+			if (index == DIM_T) {
+			  at_end = true;
+			} else {
+			  next_iterator = outer_ptr->array[index].fmbegin();
+			}
+		  }
+		}
+		return it;	
+	  }
+	  
+	  T &operator*() const {
+		return *next_iterator;
+	  }
+	  
 	};
-	// class LastDimensionMajorIterator;
+	
 	Array() {
 	  static_assert(DIM_T != 0);
 	}
@@ -73,13 +140,11 @@ namespace cs540 {
 	}
 
 	FMIterator fmbegin() {
-	  // return FMIterator(true, array);
-	  return FMIterator();
+	  return FMIterator(*this);
 	}
 
 	FMIterator fmend() {
-	  // return FMIterator(false, array);
-	  return FMIterator();
+	  return FMIterator(*this);
 	}
   };
 
@@ -88,6 +153,75 @@ namespace cs540 {
    public:
 	typedef T ValueType;
 	T array[DIM_T];
+	class FirstDimensionMajorIterator;
+	class LastDimensionMajorIterator;
+	typedef FirstDimensionMajorIterator FMIterator;
+	typedef LastDimensionMajorIterator LMIterator;
+	class FirstDimensionMajorIterator {
+	 private:
+	  friend class Array<T, DIM_T>;
+	  Array<T, DIM_T> *outer_ptr;
+	  size_t index;
+	 public:
+	  bool at_end;
+	  FirstDimensionMajorIterator(Array<T, DIM_T> &outer_class) {
+		at_end = false;
+		index = 0;
+		outer_ptr = &outer_class;
+	  }
+	  
+	  FirstDimensionMajorIterator() {
+		at_end = false;
+		index = 0;
+		outer_ptr = nullptr;
+	  }
+
+	  FirstDimensionMajorIterator(const FMIterator &other) {
+		at_end = other.at_end;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+	  }
+
+	  FirstDimensionMajorIterator &operator=(const FMIterator &other) {
+		at_end = other.at_end;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+		return *this;
+	  }
+
+	  friend bool operator==(const FMIterator &first, const FMIterator &second) {
+		return (first.index == second.index) && (first.outer_ptr == second.outer_ptr);
+	  }
+
+	  friend bool operator!=(const FMIterator &first, const FMIterator &second) {
+		return !(first == second);
+	  }
+
+	  FirstDimensionMajorIterator &operator++() {
+		if (at_end != true) {
+		  ++ index;
+		  if (index == DIM_T) {
+			at_end = true;
+		  }
+		}
+		return *this;
+	  }
+
+	  FirstDimensionMajorIterator operator++(int) {
+		auto it = *this;
+		if (at_end != true) {
+		  ++ index;
+		  if (index == DIM_T) {
+			at_end = true;
+		  }
+		}
+		return it;
+	  }
+
+	  T &operator*() const {
+		return outer_ptr->array[index];
+	  }
+	};
 	Array() {
 	  static_assert(DIM_T != 0);	  
 	}
@@ -122,6 +256,14 @@ namespace cs540 {
 	
 	T &operator[](size_t index) {
 	  return array[index];
+	}
+	
+	FMIterator fmbegin() {
+	  return FMIterator(*this);
+	}
+
+	FMIterator fmend() {
+	  return FMIterator(*this);
 	}
   };
 }
--- assignments/Assignment_4/main.cpp
+++ assignments/Assignment_4/main.cpp
@@ -4,7 +4,11 @@
 
 int main() {
   cs540::Array<int, 2, 3> array;
-  array[0][1] = 10;
+  array[0][0] = 10;
+  array[0][1] = 6;
   std::cout << array[0][1] << std::endl;
   auto it = array.fmbegin();
+  std::cout << *it << std::endl;
+  ++ it;
+  std::cout << *it << std::endl;
 }
