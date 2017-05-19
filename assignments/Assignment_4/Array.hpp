f8c85c473722e82f63066b2da2df6b0d64accf74
Rushil Kumar <rukumar333@gmail.com>
2017-05-19 15:22:59 -0400

    Fixing errors in code.

--- assignments/Assignment_4/Array.hpp
+++ assignments/Assignment_4/Array.hpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <tuple>
 #include <type_traits>
+#include <stdexcept>
 
 #ifndef ARRAY_CS_440
 #define ARRAY_CS_440
 
 namespace cs540 {
+  class OutOfRange : public std::runtime_error {
+   public:
+	OutOfRange(char const* const message) throw() : std::runtime_error(message) {}
+	virtual const char* what() const throw() {
+	  return exception::what();
+	}
+  };
+  
+  
   template <typename T, size_t... Dims> class Array;
   template <typename T, size_t DIM_T, size_t... Dims>
   class Array<T, DIM_T, Dims...> {
@@ -52,12 +62,12 @@ namespace cs540 {
 		next_iterator = other.next_iterator;
 		index = other.index;
 		outer_ptr = other.outer_ptr;
-		return &this;
+		return *this;
 	  }
 
 	  friend bool operator==(const FMIterator &first, const FMIterator &second) {
 		return (first.index == second.index) && (first.outer_ptr == second.outer_ptr)
-		  && (first.next_iterator = second.next_iterator);
+		  && (first.next_iterator == second.next_iterator);
 	  }
 
 	  friend bool operator!=(const FMIterator &first, const FMIterator &second) {
@@ -102,6 +112,91 @@ namespace cs540 {
 	  }
 	  
 	};
+
+	class LastDimensionMajorIterator {
+	 private:
+	  typedef typename NextArray::LastDimensionMajorIterator NextIterator;
+	  friend class Array<T, DIM_T, Dims...>;
+	  size_t index;
+	  NextIterator next_iterator;
+	  Array<T, DIM_T, Dims...> *outer_ptr;
+	 public:
+	  bool at_end;
+	  LastDimensionMajorIterator(Array<T, DIM_T, Dims...> &outer_class) {
+		at_end = false;
+		index = DIM_T - 1;
+		outer_ptr = &outer_class;
+		next_iterator = outer_ptr->array[index].lmbegin();
+	  }
+	  
+	  LastDimensionMajorIterator() {
+		at_end = false;
+		index = DIM_T - 1;
+		outer_ptr = nullptr;
+	  }
+	  
+	  LastDimensionMajorIterator(const FMIterator &other) {
+		at_end = other.at_end;
+		next_iterator = other.next_iterator;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+	  }
+	  
+	  LastDimensionMajorIterator &operator=(const FMIterator &other) {
+		at_end = other.at_end;
+		next_iterator = other.next_iterator;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+		return *this;
+	  }
+
+	  friend bool operator==(const LMIterator &first, const LMIterator &second) {
+		return (first.index == second.index) && (first.outer_ptr == second.outer_ptr)
+		  && (first.next_iterator == second.next_iterator);
+	  }
+
+	  friend bool operator!=(const LMIterator &first, const LMIterator &second) {
+		return !(first == second);
+	  }
+	  
+	  LastDimensionMajorIterator &operator++() {
+		if (next_iterator.at_end != true) {
+		  ++ next_iterator;
+		} else {
+		  if (at_end != true) {
+			if (index == 0) {
+			  at_end = true;
+			} else {
+			  -- index;
+			  next_iterator = outer_ptr->array[index].lmbegin();
+			}
+		  }
+		}
+		return *this;
+	  }
+	  
+	  LastDimensionMajorIterator operator++(int) {
+		auto it = *this;
+		if (next_iterator.at_end != true) {
+		  ++ next_iterator;
+		} else {
+		  if (at_end != true) {
+			if (index == 0) {
+			  at_end = true;
+			} else {
+			  -- index;
+			  next_iterator = outer_ptr->array[index].lmbegin();
+			}
+		  }
+		}
+		return it;	
+	  }
+	  
+	  T &operator*() const {
+		return *next_iterator;
+	  }
+	  
+	};
 	
 	Array() {
 	  static_assert(DIM_T != 0);
@@ -125,7 +220,8 @@ namespace cs540 {
 	Array &operator=(const Array &other) {
 	  for(size_t i = 0; i < DIM_T; ++ i) {
 		array[i] = other.array[i];
-	  }	  
+	  }
+	  return *this;
 	}
 
 	template <typename U>
@@ -136,6 +232,9 @@ namespace cs540 {
 	}
 	
 	Array<T, Dims...> &operator[](size_t index) {
+	  if (index < 0 || index >= DIM_T) {
+		throw OutOfRange("Index out of range!\n");
+	  }
 	  return array[index];
 	}
 
@@ -144,6 +243,14 @@ namespace cs540 {
 	}
 
 	FMIterator fmend() {
+	  return FMIterator(*this);
+	}
+
+	FMIterator lmbegin() {
+	  return LMIterator(*this);
+	}
+
+	FMIterator lmend() {
 	  return FMIterator(*this);
 	}
   };
@@ -222,6 +329,73 @@ namespace cs540 {
 		return outer_ptr->array[index];
 	  }
 	};
+
+	class LastDimensionMajorIterator {
+	 private:
+	  friend class Array<T, DIM_T>;
+	  Array<T, DIM_T> *outer_ptr;
+	  size_t index;
+	 public:
+	  bool at_end;
+	  LastDimensionMajorIterator(Array<T, DIM_T> &outer_class) {
+		at_end = false;
+		index = DIM_T - 1;
+		outer_ptr = &outer_class;
+	  }
+	  
+	  LastDimensionMajorIterator() {
+		at_end = false;
+		index = DIM_T - 1;
+		outer_ptr = nullptr;
+	  }
+
+	  LastDimensionMajorIterator(const FMIterator &other) {
+		at_end = other.at_end;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+	  }
+
+	  LastDimensionMajorIterator &operator=(const FMIterator &other) {
+		at_end = other.at_end;
+		index = other.index;
+		outer_ptr = other.outer_ptr;
+		return *this;
+	  }
+
+	  friend bool operator==(const LMIterator &first, const LMIterator &second) {
+		return (first.index == second.index) && (first.outer_ptr == second.outer_ptr);
+	  }
+
+	  friend bool operator!=(const LMIterator &first, const LMIterator &second) {
+		return !(first == second);
+	  }
+
+	  LastDimensionMajorIterator &operator++() {
+		if (at_end != true) {
+		  -- index;
+		  if (index == -1) {
+			at_end = true;
+		  }
+		}
+		return *this;
+	  }
+
+	  LastDimensionMajorIterator operator++(int) {
+		auto it = *this;
+		if (at_end != true) {
+		  -- index;
+		  if (index == -1) {
+			at_end = true;
+		  }
+		}
+		return it;
+	  }
+
+	  T &operator*() const {
+		return outer_ptr->array[index];
+	  }
+	};
+	
 	Array() {
 	  static_assert(DIM_T != 0);	  
 	}
@@ -245,6 +419,7 @@ namespace cs540 {
 	  for(size_t i = 0; i < DIM_T; ++ i) {
 		array[i] = other.array[i];
 	  }
+	  return *this;
 	}
 
 	template <typename U>
@@ -255,6 +430,9 @@ namespace cs540 {
 	}
 	
 	T &operator[](size_t index) {
+	  if (index < 0 || index >= DIM_T) {
+		throw OutOfRange("Index out of range!\n");
+	  }
 	  return array[index];
 	}
 	
@@ -264,6 +442,14 @@ namespace cs540 {
 
 	FMIterator fmend() {
 	  return FMIterator(*this);
+	}
+		
+	FMIterator lmbegin() {
+	  return LMIterator(*this);
+	}
+
+	FMIterator lmend() {
+	  return LMIterator(*this);
 	}
   };
 }
