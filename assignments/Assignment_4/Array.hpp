68eaa6c7d891defe7d1381751a94c53bf407df50
Rushil Kumar <rukumar333@gmail.com>
2017-05-19 13:01:38 -0400

    Trying to implement iterators.

--- assignments/Assignment_4/Array.hpp
+++ assignments/Assignment_4/Array.hpp
@@ -11,7 +11,31 @@ namespace cs540 {
   class Array<T, DIM_T, Dims...> {
    public:
 	typedef T ValueType;
-	Array<T, Dims...> array[DIM_T];
+	typedef Array<T, Dims...> NextArray;
+	NextArray array[DIM_T];
+	class FirstDimensionMajorIterator;
+	class LastDimensionMajorIterator;
+	typedef FirstDimensionMajorIterator FMIterator;
+	typedef LastDimensionMajorIterator LMIterator;
+	class FirstDimensionMajorIterator {
+	 private:
+	  // typedef typename NextArray::FirstDimensionMajorIterator NextIterator;
+	  NextArray array[DIM_T];
+	  bool at_end;
+	  // typename Array<T, Dims...>::FirstDimensionMajorIterator next_iterator;
+	 public:
+	  // FirstDimensionMajorIterator(bool begin, NextArray other_array[DIM_T]) {
+	  // 	if (begin)
+	  // 	  at_end = false;
+	  // 	else
+	  // 	  at_end = true;
+	  // 	// array = other_array;
+	  // }
+	  FirstDimensionMajorIterator() {
+		at_end = false;
+	  }
+	};
+	// class LastDimensionMajorIterator;
 	Array() {
 	  static_assert(DIM_T != 0);
 	}
@@ -46,6 +70,16 @@ namespace cs540 {
 	
 	Array<T, Dims...> &operator[](size_t index) {
 	  return array[index];
+	}
+
+	FMIterator fmbegin() {
+	  // return FMIterator(true, array);
+	  return FMIterator();
+	}
+
+	FMIterator fmend() {
+	  // return FMIterator(false, array);
+	  return FMIterator();
 	}
   };
 
--- assignments/Assignment_4/main.cpp
+++ assignments/Assignment_4/main.cpp
@@ -6,4 +6,5 @@ int main() {
   cs540::Array<int, 2, 3> array;
   array[0][1] = 10;
   std::cout << array[0][1] << std::endl;
+  auto it = array.fmbegin();
 }
